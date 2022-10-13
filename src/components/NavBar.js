9d06cf74a53eb8ded80188c503cdf277a9d130de
Mateo Gayo <mateoleo2003@gmail.com>
2022-10-12 22:06:46 -0300

    cambios de rutas

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React, { startTransition } from 'react'
+import {Link, NavLink} from 'react-router-dom'
 
 export const NavBar = () => {
     return (
     <div className="navbar bg-base-100">
         <div className="flex-1">
-            <a><img src='https://i.postimg.cc/RVh6J2g9/Captura-de-pantalla-2022-08-08-185612.jpg'/></a>
+            <Link to='/'><img src='https://i.postimg.cc/RVh6J2g9/Captura-de-pantalla-2022-08-08-185612.jpg'/></Link>
         </div>
         <div className="flex-none">
             <ul className="menu menu-horizontal p-0">
-                <li><a>Tienda</a></li>
+                <li><Link to='/tienda'>Tienda</Link></li>
                 <li tabIndex={0}>
                 <a>Parent <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg></a>
                 <ul className="p-2 bg-base-100">
